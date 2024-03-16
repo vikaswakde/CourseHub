f09fc8b0f6a1a9b737fca9944798c55cdb0ca63c
Vikas Wakde <110342308+vikaswakde@users.noreply.github.com>
2024-03-16 07:10:58 +0530

    unit added

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -20,16 +20,16 @@ export async function POST(
       where: {
         id: params.courseId,
         isPublished: true,
-      }
+      },
     });
 
     const purchase = await db.purchase.findUnique({
       where: {
         userId_courseId: {
           userId: user.id,
-          courseId: params.courseId
-        }
-      }
+          courseId: params.courseId,
+        },
+      },
     });
 
     if (purchase) {
@@ -49,9 +49,9 @@ export async function POST(
             name: course.title,
             description: course.description!,
           },
-          // unit_amount: Math.round(course.price! * 100),
-        }
-      }
+          unit_amount: Math.round(course.price!),
+        },
+      },
     ];
 
     let stripeCustomer = await db.stripeCustomer.findUnique({
@@ -60,7 +60,7 @@ export async function POST(
       },
       select: {
         stripeCustomerId: true,
-      }
+      },
     });
 
     if (!stripeCustomer) {
@@ -72,25 +72,25 @@ export async function POST(
         data: {
           userId: user.id,
           stripeCustomerId: customer.id,
-        }
+        },
       });
     }
 
     const session = await stripe.checkout.sessions.create({
       customer: stripeCustomer.stripeCustomerId,
       line_items,
-      mode: 'payment',
+      mode: "payment",
       success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?success=1`,
       cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?canceled=1`,
       metadata: {
         courseId: course.id,
         userId: user.id,
-      }
+      },
     });
 
     return NextResponse.json({ url: session.url });
   } catch (error) {
     console.log("[COURSE_ID_CHECKOUT]", error);
-    return new NextResponse("Internal Error", { status: 500 })
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
